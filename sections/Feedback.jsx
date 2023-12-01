d5204b23a76adbc8f5cd47c151d240410ced63ba
leanmaga <thesharkcommunity@gmail.com>
2023-12-01 11:41:03 -0300

    mejorar seccion feedback

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -5,85 +5,65 @@ import { motion } from 'framer-motion';
 import styles from '../styles';
 import { fadeIn, staggerContainer, zoomIn } from '../utils/motion';
 
-import { NewFeatures, TitleText, TypingText } from '../components';
-
-import { ContactForm } from '../components';
-
 const Feedback = () => (
-  <section className={`${styles.paddings} relative z-10`}>
-
+  <section className={`${styles.paddings}`}>
     <motion.div
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
       viewport={{ once: false, amount: 0.25 }}
-      className={`${styles.innerWidth} min-h-[100vh] flex flex-row gap-8 justify-center content-center flex-wrap`}
-      >   
+      className={`${styles.innerWidth} mx-auto w-full flex flex-row xs:flex-col gap-6`}
+    >
+      <motion.div
+        variants={fadeIn('right', 'tween', 0.2, 1)}
+        className="flex-[0.5] max-w-[370px] justify-center flex-col gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
+      >
+        <div className="feedback-gradient" />
+        <div>
+          <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
+              Artesanias Stitch 
+          </h4>
+          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
+              Experiencia Excepcional con el Servicio de PatagoniaScript
+          </p>
+        </div>
+
+        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
+                “Contratar a PatagoniaScript fue una decisión acertada. Su equipo entendió perfectamente mis necesidades como ingeniero. 
+                La landing page que crearon fue precisa, estéticamente agradable y cumplió con todos mis requisitos técnicos. 
+                La comunicación fue fluida, y aprecio la transparencia y la puntualidad en cada fase del proyecto. Recomiendo 
+                encarecidamente sus servicios a cualquier profesional que busque calidad y eficiencia.”
+        </p>
+      </motion.div>
+
+      <motion.div
+        variants={fadeIn('left', 'tween', 0.2, 1)}
+        className="relative flex-1 flex justify-center items-center"
+      >
+        <img
+          src="/cumbre02.jpg"
+          alt="planet-09"
+          className="max-h-[610px] h-auto min-h-[210px] object-cover rounded-[40px]"
+        />
+
         <motion.div
-          variants={fadeIn('right', 'tween', 0.2, 1)}
-          className="flex flex-col justify-center items-center text-center content-center flex-wrap"
+          variants={zoomIn(0.4, 1)}
+          className="absolute -left-[10%] top-[3%] z-11"
         >
-          <TypingText title="| Feedbacks" />
-
-          <TitleText title={<>Nuestros clientes</>} />
-          
-          <div>
-            
-            <motion.div
-              variants={fadeIn('right', 'tween', 0.2, 1)}
-              className="flex-[0.5] lg:max-w-[370px] flex gradient-05 sm:p-8 p-4 rounded-[32px] border-[1px] border-[#6A6A6A] relative"
-            >
-              <div className="feedback-gradient" />
-
-              <div>
-                <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
-                  Artesanias Stitch 
-                </h4>
-                <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
-                  Experiencia Excepcional con el Servicio de PatagoniaScript
-                </p>
-              </div>
-              
-              <p className="mt-[24px] font-normal sm:text-[18px] text-[12px] sm:leading-[45.6px] leading-[39.6px] text-white">
-              “Contratar a PatagoniaScript fue una decisión acertada. Su equipo entendió perfectamente mis necesidades como ingeniero. La landing page que crearon fue precisa, estéticamente agradable y cumplió con todos mis requisitos técnicos. La comunicación fue fluida, y aprecio la transparencia y la puntualidad en cada fase del proyecto. Recomiendo encarecidamente sus servicios a cualquier profesional que busque calidad y eficiencia.”
-              </p>
-
-              <motion.div
-              variants={fadeIn('left', 'tween', 0.2, 1)}
-              className="relative flex-1 flex justify-center items-center"
-            >
-              <img
-                src="/cumbre02.jpg"
-                alt="planet-09"
-                className="max-w-[500px] w-full h-auto object-cover rounded-[40px]"
-              />
-
-              <motion.div
-                variants={zoomIn(0.4, 1)}
-                className="lg:block hidden absolute -left-[10%] top-[3%]"
-              >
-                <img
-                  src="/react-2.svg"
-                  alt="stamp"
-                  className="w-[155px] h-[155px] object-contain"
-                />
-              </motion.div>
-              </motion.div>
-
-
-            </motion.div>
-
-                
-          </div>
-
-          <ContactForm/>
-
-
+          <img
+            src="/react-2.svg"
+            alt="stamp react"
+            className="w-[155px] h-[155px] object-contain"
+          />
         </motion.div>
-
-        
+      </motion.div>
     </motion.div>
   </section>
 );
 
 export default Feedback;
+
+
+
+
+
